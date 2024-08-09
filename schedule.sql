fe55af6d64dd29cc576a0c6bfb281a446ce8f48f
ik <jeongik11029@naver.com>
2024-08-10 02:08:04 +0900

    레벨 0: schedule.sql 작성 -> schedule, admin 테이블 생성 쿼리; build.gradle - implementation 'org.springframework.boot:spring-boot-starter-jdbc' 추가 -> jdbc 라이브러리

---
+++ schedule.sql
@@ -0,0 +1,20 @@
+create table schedule
+(
+    schedule_id       varchar(36),
+    schedule_password varchar(10),
+    task              varchar(20),
+    admin_name        varchar(10),
+    registration_date Date,
+    modification_date Date,
+    primary key (schedule_id)
+);
+
+create table admin
+(
+    admin_id varchar(36),
+    admin_name varchar(10),
+    email varchar(50),
+    registration_date Date,
+    modification_date Date,
+    primary key (admin_id)
+);
