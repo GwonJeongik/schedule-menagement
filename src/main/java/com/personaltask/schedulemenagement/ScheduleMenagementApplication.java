77fe797352deaf3708f25a7696e6da758981edad
ik <jeongik11029@naver.com>
2024-08-09 17:13:47 +0900

    Initial commit

---
+++ src/main/java/com/personaltask/schedulemenagement/ScheduleMenagementApplication.java
@@ -0,0 +1,13 @@
+package com.personaltask.schedulemenagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleMenagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleMenagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personaltask/schedulemenagement/ScheduleMenagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personaltask.schedulemenagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleMenagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
